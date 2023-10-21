5dbfd6dc0f6ed27b7472b7d27215aa2a37dbc3fe
Roman Ihnatov <tov.bk.pavuk@gmail.com>
2023-10-20 20:06:42 -0700

    homa task 6 and 7

--- script.js
+++ script.js
@@ -1,42 +1,82 @@
-// Home Task 3
-const line_1 = prompt("Enter a line", "line_1")
-const line_2 = prompt("Enter a line", "line_2")
-const line_3 = prompt("Enter a line", "line_3")
+// Home task 6
+const questionDOB = "Can you please provide your born year?"
+const questionPlace = "What is the name of the place you live in?"
+const questionSport = "What is your favorite sport?"
+const refuseToEnter = "It's a pity that you didn't enter"
 
-alert(line_1 + line_2 + line_3)
+let enteredText = prompt(questionDOB, "")
+const dateOfBirth = (enteredText === null) ? (alert(`${refuseToEnter} your birth year.`), "") : enteredText
 
-let newParagraph = document.createElement("p")
-newParagraph.innerText = `You entered the text: ${line_1 + line_2 + line_3}`
-document.body.appendChild(newParagraph)
-const numbers = "12345"
+enteredText = prompt(questionPlace, "")
+const placeOfResidence = (enteredText === null) ? (alert(`${refuseToEnter} the place you live in.`), "") : enteredText
 
-let numbersParagraph = document.createElement("p")
-numbersParagraph.innerText = `Number is: ${numbers}`
-document.body.appendChild(numbersParagraph)
+enteredText = prompt(questionSport, "")
+const favoriteSport = (enteredText === null) ? (alert(`${refuseToEnter} your favorite sport.`), "") : enteredText
 
-let arr = [...numbers]
+let person = {
+    place: "",
+    championName: "",
+    sport: favoriteSport,
+};
 
-for (let num of arr) {
-    let newParagraph = document.createElement("p")
-    newParagraph.innerText = `${num}`
-    document.body.appendChild(newParagraph)
+switch (placeOfResidence.toLowerCase()) {
+    case "kiev":
+        person["place"] = "Ukraine";
+        break;
+    case "washington":
+        person["place"] = "USA";
+        break;
+    case "london":
+        person["place"] = "England";
+        break;
+    default:
+        person["place"] = placeOfResidence;
 }
 
-// Home Task 4
+switch (favoriteSport.toLowerCase()) {
+    case "box":
+        person["championName"] = "Muhammad Ali";
+        break;
+    case "diving":
+        person["championName"] = "Greg Louganis";
+        break;
+    case "ski":
+        person["championName"] = "Ingemar Stenmark";
+        break;
+    case "":
+        person["championName"] = null;
+        break;
+}
 
-const first_number = prompt("first_number", "0")
-const second_number = prompt("second_number", "1")
+const placeMessage = person.place ? `You live in ${person.place}` : "You don't have a home";
+const sportMessage = person.championName ? `Do you want to become ${person.championName}?` :
+    person.sport ? `So you like ${person.sport}` : "You don't like sport";
+const ageMessage = dateOfBirth < 1913 ? "IT is hard to believe you exist" : dateOfBirth ? `Your age is near ${2023 - dateOfBirth}` : "You don't exist"
+person.placeMessage = placeMessage;
+person.sportMessage = sportMessage;
+person.ageMessage = ageMessage;
 
 alert(
-    `
-    ${first_number} + ${second_number} = ${Number(first_number) + Number(second_number)}
-    ${first_number} - ${second_number} = ${first_number - second_number}
-    ${first_number} * ${second_number} = ${first_number * second_number}
-    ${first_number} / ${second_number} = ${first_number / second_number}
-    `
-)
+    `${person.ageMessage}\n
+    ${person.placeMessage}\n
+    ${person.sportMessage}\n
+    `)
 
-// Home Task 5
-const hoursAmount = prompt("Enter hours amount", "1")
-let seconds = Number(hoursAmount) * 3600
-alert(`Seconds = ${seconds}`)
+
+// Home task 7
+let numOrStr = prompt('input number or string');
+
+switch (true) {
+    case numOrStr == null:
+        console.log("You pressed \"Cancel\"");
+        break;
+    case numOrStr.trim() === "":
+        console.log('Empty String');
+        break;
+    case numOrStr !== "" && numOrStr.length > 0 && isNaN(Number(numOrStr)):
+        console.log(`string = ${numOrStr}`);
+        break;
+    default:
+        console.log(`number = ${numOrStr}`);
+        break;
+}
