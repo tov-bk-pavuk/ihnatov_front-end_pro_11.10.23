b75c1d7e47f9879c70704563c0c16fa456344af5
Roman Ihnatov <tov.bk.pavuk@gmail.com>
2023-12-03 23:50:33 -0800

    add Home task 21

---
+++ script_ht_21.js
@@ -0,0 +1,51 @@
+// Створити програму для відображення результатів голосування. З наступними умовами:
+//
+// Як варіанти відповіді - смайлики. За замовчуванням, 5шт.
+// Виведення даних - смайлики в один рядок, під ними - кількість тих, хто проголосував за кожен смайл.
+// При натисканні на смайл - під ним змінюється значення лічильника.
+// Реалізувати таким чином, щоб додавання нових варіантів відповіді не вело до додавання нових функцій.
+
+smiles = (number) => (
+        {
+        1: "img/smile_1.jpg",
+        2: "img/smile_2.jpg",
+        3: "img/smile_3.jpg",
+        4: "img/smile_4.jpg",
+        5: "img/smile_5.jpg",
+}[number]
+)
+
+polls = {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+}
+
+const body = document.querySelector("body")
+const div = document.createElement("div")
+div.id = "mainDiv"
+div.style.display = "flex"
+div.style.marginLeft = "auto"
+
+const length = Object.keys(polls).length;
+for (let number = 1; number <= length; number++) {
+        const inner_div = document.createElement("div")
+        inner_div.style.width = "20%"
+        inner_div.style.padding = "10px"
+        inner_div.id = number.toString()
+        const img = document.createElement("img")
+        const innerParagraph = document.createElement("p")
+        img.src = smiles(number)
+        inner_div.appendChild(img)
+        inner_div.appendChild(innerParagraph)
+        div.appendChild(inner_div)
+}
+
+body.appendChild(div)
+
+document.querySelector("#mainDiv").addEventListener("click", function (event) {
+        polls[event.target.parentNode.id]++
+        event.target.parentNode.children[1].textContent = polls[event.target.parentNode.id]
+})
